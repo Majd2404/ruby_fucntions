361c0a6c673a036e880fb09fcb47065dc617edc0
Majd Machlouch <56276308+Majd2404@users.noreply.github.com>
2023-10-25 10:25:15 +0100

    Comment code

--- Counting_all_occurrences_of_digits_in_a_string.rb
+++ Counting_all_occurrences_of_digits_in_a_string.rb
@@ -1,5 +1,11 @@
+# Get the number of lines
 n = gets.to_i
+
+# Process each line
 n.times do
+  # Read a line of input and remove trailing newline
   row = gets.chomp
+  
+  # Count and output the number of digits in the current line
   puts row.count('0123456789')
 end
